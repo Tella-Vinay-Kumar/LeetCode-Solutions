1f21282377756696d738b96bfcf15f81708e06fa
Tella-Vinay-Kumar <vinaytella01@gmail.com>
2025-07-18 21:41:09 +0530

    FloorOfANumber

--- BinarySearchProblems/FloorOfANumber.java
+++ BinarySearchProblems/FloorOfANumber.java
@@ -1,4 +1,6 @@
- class Solution{
+problem: given an sorted array and a target find all occurances of that tareget in the array which might contain duplicate elements
+
+class Solution{
     public int floor(int[] arr, int target) {
         int start =0,end=arr.length-1,ans=-1;
         while(start<=end)
@@ -18,3 +20,4 @@
         }
     }return ans;
 }
+
