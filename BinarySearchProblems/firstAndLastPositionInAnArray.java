68c9b128f6640267bb598921c76eee6179493b5d
Tella-Vinay-Kumar <vinaytella01@gmail.com>
2025-07-18 20:32:41 +0530

    Add files via upload

---
+++ BinarySearchProblems/firstAndLastPositionInAnArray.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+
+        int ans[] = new int[2];
+        ans[0]=binarySearch(nums,  target, true);
+        ans[1]= binarySearch(nums, target,  false);
+        return ans;
+    }
+    public static int binarySearch(int nums[], int target, boolean isFirst){
+        int start=0, end=nums.length-1,ans=-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(nums[mid]==target){
+                ans=mid;
+                if(isFirst){
+                    end=mid-1;
+                }else{
+                    start = mid+1;
+                }
+            } else if(target > nums[mid]){
+                start=mid+1;
+            } else{
+                end = mid-1;
+            }
+        }
+        return ans;
+
+    }
+}
